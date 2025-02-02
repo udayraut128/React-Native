46bafd05a950f6b745f980fa991667d47f1fb1dc
coderroot2 <udayraut128@gmail.com>
2025-02-03 01:19:43 +0530

    add todo

---
+++ Big_Project/Todos/app/index.jsx
@@ -0,0 +1,17 @@
+import { Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {Link} from "expo-router";
+import "../global.css";
+
+export default function App() {
+  return (
+    <View className="flex-1 justify-center items-center bg-blue-500">
+      <Text className="text-3xl">Hello</Text>
+      
+       
+    </View>
+  );
+};
+
+ 
